de8a5de177579051c87160a3575c0685dcdf8a7d
Chrisgarcia164 <91617441+Chrisgarcia164@users.noreply.github.com>
2021-12-08 19:30:22 -0500

    connected static public folder to express and now sends the index.html to the homepage

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+app.use(express.static("public"));
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.send(index.html);
 });
 
 app.listen(PORT, () => {
