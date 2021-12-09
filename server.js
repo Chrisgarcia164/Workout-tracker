2e8d0d41dbf4d0a8a745c9951b47c78a2461b78e
Chrisgarcia164 <91617441+Chrisgarcia164@users.noreply.github.com>
2021-12-08 19:28:38 -0500

    created server and sends hello world to the main page

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
 app.listen(PORT, () => {
-  console.log(`http/localhost:3000`);
+  console.log(`http://localhost:3000`);
 });
