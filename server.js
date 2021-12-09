9b4fa797f4453ae5ba992fbd3cfd2c08535b06fa
Chrisgarcia164 <91617441+Chrisgarcia164@users.noreply.github.com>
2021-12-08 19:43:48 -0500

    added file paths for homepage, exercise and stats endpoints

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const PORT = 3000;
 
 app.use(express.static("public"));
-app.get("/", (req, res) => {
-  res.send(index.html);
-});
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+app.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/exercise.html"));
+});
+app.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/stats.html"));
+});
 app.listen(PORT, () => {
   console.log(`http://localhost:3000`);
 });
